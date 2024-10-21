330aea980ec6eba42812e01aea898f23d123532a
Clément Sannier <clement.sannier@digitalcrafts.fr>
2024-10-21 12:02:55 +0200

    test(GlobalTest): :white_check_mark: Add Global Verify Test

---
+++ LordOfTheRings.Tests/GlobalProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using LordOfTheRings.App;
+using Xunit;
+
+namespace LordOfTheRings.Tests;
+
+public sealed class GlobalProgramTest
+{
+	[Fact]
+	public async Task Should_Retrieve_Initial_Result()
+	{
+		var stringWriter = new StringWriter();
+		Console.SetOut(stringWriter);
+		FellowshipOfTheRingApp.Run();
+		var output = stringWriter.ToString();
+		Console.SetOut(Console.Out);
+		await Verify(output);
+	}
+}
